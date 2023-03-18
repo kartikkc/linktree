8210c768f413e73862b6f7a546a952130215689a
Kartik <kartikkc397@outlook.com>
2023-03-18 10:34:30 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+// import logo from './logo.svg';
+import './App.css';
+
+const App = () => {
+  return (
+    <>
+      
+    {/* Footer */}
+      <div class="waveWrapper waveAnimation">
+        <div class="waveWrapperInner bgTop">
+          <div class="wave waveTop"></div>
+      </div>
+      <div class="waveWrapperInner bgMiddle">
+        <div class="wave waveMiddle"></div>
+      </div>
+      <div class="waveWrapperInner bgBottom">
+        <div class="wave waveBottom"></div>
+      </div>
+    </div>
+    {/* Footer-end */}
+      </>
+      );
+};
+
+export default App;
